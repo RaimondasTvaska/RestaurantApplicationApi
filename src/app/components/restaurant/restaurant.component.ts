b6f55e5423ecfdb024483323cc188fa562a21b41
RaimondasTvaska <rtvaska@gmail.com>
2021-11-01 21:07:23 +0200

    ver 20211101

--- src/app/components/restaurant/restaurant.component.ts
+++ src/app/components/restaurant/restaurant.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Menu } from 'src/app/models/menu';
 import { Restaurant } from 'src/app/models/restaurant';
+import { MenuService } from 'src/app/services/menu.service';
 import { RestaurantService } from 'src/app/services/restaurant.service';
 
 @Component({
@@ -11,6 +12,7 @@ import { RestaurantService } from 'src/app/services/restaurant.service';
 export class RestaurantComponent implements OnInit {
 
   private restaurantService : RestaurantService;
+  private menuService : MenuService;
 
   public restaurants : Restaurant[] = [];
   public menus : Menu[] = [];
@@ -23,14 +25,19 @@ export class RestaurantComponent implements OnInit {
 
   public editMode : boolean = false;
   
-  constructor(restaurantService : RestaurantService) {
+  constructor(restaurantService : RestaurantService, menuService : MenuService) {
     this.restaurantService = restaurantService;
+    this.menuService = menuService;
    }
 
   ngOnInit(): void {
     this.restaurantService.getAllRestaurants().subscribe((restaurantFromApi) =>{
       this.restaurants = restaurantFromApi;
+      this.restaurants.sort((a, b) => a.name.localeCompare(b.name))
   })
+    this.menuService.getAllMenu().subscribe((menuFromApi) =>{
+      this.menus = menuFromApi;
+    })
 }
 public addRestaurant(){
   var newRestaurant : Restaurant = {
@@ -42,8 +49,50 @@ public addRestaurant(){
   }
   this.restaurantService.addRestaurant(newRestaurant).subscribe((restaurantId) =>{
     newRestaurant.id = restaurantId;
-    this.restaurants.push(newRestaurant);
+    this.restaurants.push(newRestaurant);    
   })
+}
+public deleteRestaurant(id: number) : void {
+  this.restaurantService.deleteRestaurant(id).subscribe(() =>{   
+  this.restaurants = this.restaurants.filter(h => h.id !== id);
+  })
+}
+public updateRestaurant(restaurant: Restaurant): void {
+  this.editMode = true;
+
+  this.id = restaurant.id;
+  this.name = restaurant.name;
+  this.customers = restaurant.customers;
+  this.employees = restaurant.employees;
+  this.menuId = restaurant.menuId;
+}
+
+public sendUpdatedRestaurant (){
+  var updatedRestaurant: Restaurant = {
+    id: this.id,
+    name : this.name,
+    customers : this.customers,
+    employees : this.employees,
+    menuId : this.menuId,
+  }
+  this.restaurantService.updateRestaurant(updatedRestaurant).subscribe(() =>{
+    // for (let i = 0; i < this.restaurants.length; i++) {
+    //   const emp = this.restaurants[i];
+    //   if (emp.id == updatedRestaurant.id) {
+    //     emp.name = updatedRestaurant.name;
+    //     emp.customers = updatedRestaurant.customers;          
+    //     emp.employees = updatedRestaurant.employees;          
+    //     emp.menuId = updatedRestaurant.menuId;
+    //     return;          
+    //   }      
+    // } 
+    (this.restaurants = this.restaurants.map((e) =>
+    e.id !== updatedRestaurant.id ? e : updatedRestaurant
+    )
+    .sort((a, b) => a.name.localeCompare(b.name))
+    )   
+  })
+  this.editMode = false;  
 }
 
 }
--- src/app/services/restaurant.service.ts
+++ src/app/services/restaurant.service.ts
@@ -20,4 +20,10 @@ export class RestaurantService {
   public addRestaurant(restaurant : Restaurant): Observable<number>{
     return this.http.post<number>("https://localhost:44328/api/Restaurant", restaurant)
   }
+  public deleteRestaurant(id: number): Observable<Restaurant> {
+    return this.http.delete<Restaurant>(`https://localhost:44328/api/Restaurant/${id}`);
+  }
+  public updateRestaurant(updatedRestaurant:Restaurant): Observable<Restaurant> {
+    return this.http.put<Restaurant>("https://localhost:44328/api/Restaurant", updatedRestaurant);
+  }
 }
